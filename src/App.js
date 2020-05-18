a44ee9b1192fb1d727b14181895cf4ac32beb133
SaraBlich <s.blich@gmail.com>
2020-05-18 18:05:01 +0200

    changes added. footer and header exploration done

--- src/App.js
+++ src/App.js
@@ -1,44 +1,21 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import ToggleMenu from './components/ToggleMenu';
 
 import './App.css';
 
-class App extends Component
+function App()
 {
-  constructor(props)
-  {
-      super(props);
-      this.state =
-      {
-          listOn: false
-      };
 
-
-   this.handleToggle = this.handleToggle.bind(this);
-  }
-
-  handleToggle()
-  {
-      this.setState({
-          listOn: !this.state.listOn
-      });
-  }
-  render()
-  {
   return (
     <div className="App">
       <Header />
       <main style = {{marginTop: '20vh'}}/>
-      {this.state.listOn ? <ToggleMenu 
-      handleToggle = {this.handleToggle}
-      /> : undefined}
       CONTENT
       <Footer />
     </div>
   );
   }
-}
+
 
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,30 @@
-import React, {Fragment} from 'react';
+import React, {Fragment, Component} from 'react';
 import ToggleMenu from '../components/ToggleMenu';
 import ToggleList from '../components/ToggleList';
 
 import '../css/header-styles.css';
 import logo from '../images/Team-red.png';
 
-function Header () {
+class Header extends Component {
+  constructor(props)
+  {
+      super(props);
+      this.state =
+      {
+          listOn: true
+      };
+
+
+   this.handleToggle = this.handleToggle.bind(this);
+  }
+
+  handleToggle()
+  {
+      this.setState({
+          listOn: !this.state.listOn
+      });
+  }
+  render(){
     return (
       <Fragment>
         <div className='header-container'>
@@ -22,13 +41,14 @@ function Header () {
                   </div>
             </nav>
             <nav className = 'header-toggle-menu'>
-             <ToggleMenu/>
+             <ToggleMenu handleToggle = {this.handleToggle}/>
             </nav>
         </div>
-        <ToggleList/>
+        {this.state.listOn ? <ToggleList/> : undefined}
 
       </Fragment>
     );
   }
+}
 
   export default Header;
