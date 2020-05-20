c4acc8894d752102dd15fc85b2c33fcec6ec15e2
SaraBlich <s.blich@gmail.com>
2020-05-20 15:53:21 +0200

    bug fixed in memberslist

--- src/components/MembersList.js
+++ src/components/MembersList.js
@@ -4,13 +4,14 @@ import React from 'react';
 const MemberList = ({ membersInfo }) => {
     return (
         <>
-            <img src ={membersInfo.image} alt = 'members' />
+            
             <div className="members-list">
                 Name: {membersInfo.name}
                 Description: {membersInfo.description}
-                {console.log(membersInfo)}
+                <img src ={membersInfo.image} alt = 'members' />
+
             </div>
-            
+
         </>
     )
 }
