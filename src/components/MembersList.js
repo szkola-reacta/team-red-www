70b4a9454386e9582204bc73b0f55272d344617c
Wojtek <gggolabek@gmail.com>
2020-05-20 16:56:18 +0200

    add GitHub and LinkedIn buttons

--- src/components/MembersList.js
+++ src/components/MembersList.js
@@ -4,14 +4,13 @@ import React from 'react';
 const MemberList = ({ membersInfo }) => {
     return (
         <>
-            
             <div className="members-list">
                 Name: {membersInfo.name}
                 Description: {membersInfo.description}
                 <img src ={membersInfo.image} alt = 'members' />
-
+                <button><a href={membersInfo.githubURL}>GIT HUB</a></button>
+                <button><a href={membersInfo.linkedIN}>LinkedIN</a></button>
             </div>
-
         </>
     )
 }
