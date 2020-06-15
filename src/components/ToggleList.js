6037a2e988b29e66a20d93a3cc1dada6342270b9
SaraBlich <s.blich@gmail.com>
2020-06-15 14:05:21 +0200

    togglelist fixed, css fixed

--- src/components/ToggleList.js
+++ src/components/ToggleList.js
@@ -7,9 +7,9 @@ function ToggleList()
     return(
         <div className = 'togglelist-container'>
             <ul className = 'ul-toggle'>
-                <li className = 'li-toggle'><a href ='/'>Home</a></li>
-                <li className = 'li-toggle'><a href = '/'>About</a></li>
-                <li className = 'li-toggle'><a href = '/'>Team</a></li>
+                <li className = 'li-toggle'><a href ='#home'>Home</a></li>
+                <li className = 'li-toggle'><a href = '#team'>Team</a></li>
+                <li className = 'li-toggle'><a href = '#projects-list'>Project</a></li>
             </ul>
         </div>
     )
